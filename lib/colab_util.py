37d1c072856e50efe3cc45fd86bb77a094e57276
End18 <bangyh@gmail.com>
2020-09-20 20:43:27 +0900

    Update colab_util.py
    apply latest change

--- lib/colab_util.py
+++ lib/colab_util.py
@@ -1,18 +1,14 @@
 '''
 MIT License
-
 Copyright (c) 2019 Shunsuke Saito, Zeng Huang, and Ryota Natsume
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,61 +20,45 @@ SOFTWARE.
 import io
 import os
 import torch
-from torch import nn
 from skimage.io import imread
 import numpy as np
 import cv2
-from tqdm import tqdm
+from tqdm import tqdm_notebook as tqdm
 import base64
+from IPython.display import HTML
 
 # Util function for loading meshes
 from pytorch3d.io import load_objs_as_meshes
 
+from IPython.display import HTML
 from base64 import b64encode
 
 # Data structures and functions for rendering
-from pytorch3d.structures import Meshes, Textures
+from pytorch3d.structures import Meshes
 from pytorch3d.renderer import (
     look_at_view_transform,
-    OpenGLOrthographicCameras,
-    SfMPerspectiveCameras,
+    OpenGLOrthographicCameras, 
     PointLights, 
     DirectionalLights, 
     Materials, 
     RasterizationSettings, 
     MeshRenderer, 
     MeshRasterizer,  
-    TexturedSoftPhongShader,
-    HardPhongShader
+    HardPhongShader,
+    TexturesVertex
 )
 
-
-class MeshRendererWithDepth(nn.Module):
-    def __init__(self, rasterizer, shader):
-        super().__init__()
-        self.rasterizer = rasterizer
-        self.shader = shader
-
-    def forward(self, meshes_world, **kwargs) -> torch.Tensor:
-        fragments = self.rasterizer(meshes_world, **kwargs)
-        images = self.shader(fragments, meshes_world, **kwargs)
-        return images, fragments.zbuf
-
-
-def set_renderer(image_size=512, use_sfm=False):
+def set_renderer():
     # Setup
     device = torch.device("cuda:0")
     torch.cuda.set_device(device)
 
     # Initialize an OpenGL perspective camera.
-    R, T = look_at_view_transform(2.0, 0, 180)
-    if use_sfm:
-        cameras = SfMPerspectiveCameras(focal_length=580.0, device=device, R=R, T=T)
-    else:
-        cameras = OpenGLOrthographicCameras(device=device, R=R, T=T)
+    R, T = look_at_view_transform(2.0, 0, 180) 
+    cameras = OpenGLOrthographicCameras(device=device, R=R, T=T)
 
     raster_settings = RasterizationSettings(
-        image_size=image_size,
+        image_size=512, 
         blur_radius=0.0, 
         faces_per_pixel=1, 
         bin_size = None, 
@@ -87,12 +67,17 @@ def set_renderer(image_size=512, use_sfm=False):
 
     lights = PointLights(device=device, location=((2.0, 2.0, 2.0),))
 
-    rasterizer = MeshRasterizer(cameras=cameras, raster_settings=raster_settings)
-    shader = HardPhongShader(device=device, cameras=cameras, lights=lights)
-    if use_sfm:
-        renderer = MeshRendererWithDepth(rasterizer=rasterizer, shader=shader)
-    else:
-        renderer = MeshRenderer(rasterizer=rasterizer, shader=shader)
+    renderer = MeshRenderer(
+        rasterizer=MeshRasterizer(
+            cameras=cameras, 
+            raster_settings=raster_settings
+        ),
+        shader=HardPhongShader(
+            device=device, 
+            cameras=cameras,
+            lights=lights
+        )
+    )
     return renderer
 
 def get_verts_rgb_colors(obj_path):
@@ -109,7 +94,7 @@ def get_verts_rgb_colors(obj_path):
 
 def generate_video_from_obj(obj_path, image_path, video_path, renderer):
     input_image = cv2.imread(image_path)
-    # input_image = input_image[:,:input_image.shape[1]//3]
+    input_image = input_image[:,:input_image.shape[1]//3]
     input_image = cv2.resize(input_image, (512,512))
 
     # Setup
@@ -119,8 +104,8 @@ def generate_video_from_obj(obj_path, image_path, video_path, renderer):
     # Load obj file
     verts_rgb_colors = get_verts_rgb_colors(obj_path)
     verts_rgb_colors = torch.from_numpy(verts_rgb_colors).to(device)
-    textures = Textures(verts_rgb=verts_rgb_colors)
-    wo_textures = Textures(verts_rgb=torch.ones_like(verts_rgb_colors)*0.75)
+    textures = TexturesVertex(verts_features=verts_rgb_colors)
+    # wo_textures = TexturesVertex(verts_features=torch.ones_like(verts_rgb_colors)*0.75)
 
     # Load obj
     mesh = load_objs_as_meshes([obj_path], device=device)
@@ -129,10 +114,10 @@ def generate_video_from_obj(obj_path, image_path, video_path, renderer):
     vers = mesh._verts_list
     faces = mesh._faces_list
     mesh_w_tex = Meshes(vers, faces, textures)
-    mesh_wo_tex = Meshes(vers, faces, wo_textures)
+    # mesh_wo_tex = Meshes(vers, faces, wo_textures)
 
     # create VideoWriter
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2. VideoWriter_fourcc(*'MP4V')
     out = cv2.VideoWriter(video_path, fourcc, 20.0, (1024,512))
 
     for i in tqdm(range(90)):
@@ -144,3 +129,8 @@ def generate_video_from_obj(obj_path, image_path, video_path, renderer):
         image = np.concatenate([input_image, images_w_tex], axis=1)
         out.write(image.astype('uint8'))
     out.release()
+
+def video(path):
+    mp4 = open(path,'rb').read()
+    data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
+    return HTML('<video width=500 controls loop> <source src="%s" type="video/mp4"></video>' % data_url)
